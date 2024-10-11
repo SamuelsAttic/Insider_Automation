feb577723e340de495f480204df63418b0aef98f
SamQAie <168630039+SamQAie@users.noreply.github.com>
2024-10-11 16:45:10 +0200

    Step 4 Updates

--- IncompleteSelenium/pages/jobs_page.py
+++ IncompleteSelenium/pages/jobs_page.py
@@ -1,9 +1,13 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+import time
 
 class JobsPage(BasePage):
-    LOCATION_FILTER = (By.ID, "filter-by-location")
-    DEPARTMENT_FILTER = (By.ID, "filter-by-department")
+    LOCATION_FILTER = (By.XPATH, f"//form[@id='top-filter-form']/div/span/span/span/span[2]/b") 
+    DEPARTMENT_FILTER = (By.XPATH, f"//form[@id='top-filter-form']/div[2]/span/span/span/span[2]/b")
     JOB_LIST = (By.CLASS_NAME, "position-list-item")
     QA_JOBS_BUTTON = (By.CLASS_NAME, "btn.btn-outline-secondary.rounded.text-medium.mt-2.py-3.px-lg-5.w-100.w-md-50")
 
@@ -12,12 +16,15 @@ class JobsPage(BasePage):
         self.driver.get("https://useinsider.com/careers/quality-assurance/")
 
     def filter_jobs(self, location="Istanbul, Turkey", department="Quality Assurance"):
-        self.driver.find_element(By.ID, "wt-cli-accept-all-btn").click()
-        self.driver.find_element(*self.LOCATION_FILTER).click()
-        self.driver.find_element(By.XPATH, f"//span[text()='{location}']").click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, "wt-cli-accept-all-btn"))).click()
 
-        self.driver.find_element(*self.DEPARTMENT_FILTER).click()
-        self.driver.find_element(By.XPATH, f"//span[text()='{department}']").click()
+        time.sleep(20)
+        
+        actions = ActionChains(self.driver)
+        actions.move_to_element(self.driver.find_element(*self.LOCATION_FILTER)).click().perform()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[id*='Istanbul, Turkey']"))).click()
+        actions.move_to_element(self.driver.find_element(*self.DEPARTMENT_FILTER)).click().perform()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[id*='Quality Assurance']"))).click()
 
     def are_jobs_present(self):
         return len(self.driver.find_elements(*self.JOB_LIST)) > 0
@@ -25,15 +32,16 @@ class JobsPage(BasePage):
     def validate_jobs(self, location, department):
         jobs = self.driver.find_elements(*self.JOB_LIST)
         for job in jobs:
-            job_location = job.find_element(By.CLASS_NAME, "location").text
-            job_department = job.find_element(By.CLASS_NAME, "department").text
-            job_position = job.find_element(By.CLASS_NAME, "title").text
+            job_location = job.find_element(By.CSS_SELECTOR, "[class*='position-location']").text
+            job_department = job.find_element(By.CSS_SELECTOR, "[class*='position-department']").text
+            job_position = job.find_element(By.CSS_SELECTOR, "[class*='position-title']").text
             if location not in job_location or department not in job_department or department not in job_position:
                 return False
         return True
 
     def view_first_job_role(self):
-        self.driver.find_elements(*self.JOB_LIST)[0].find_element(By.LINK_TEXT, "View Role").click()
+        currentElement = self.driver.find_elements(*self.JOB_LIST)[0]
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((currentElement.find_element(By.CLASS_NAME, "btn.btn-navy.rounded.pt-2.pr-5.pb-2.pl-5")))).click()
 
     def go_to_qa_jobs(self):
         self.driver.find_element(*self.QA_JOBS_BUTTON).click()
